6dd44de75576db56b23c3f1cca96ad73a889f5cb
maekawa-kazuma <kazumaekawa7218@gmail.com>
2022-03-22 20:35:01 +0900

    pumaファイルの修正

--- config/puma.rb
+++ config/puma.rb
@@ -6,7 +6,7 @@
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+workers Integer(ENV.fetch("WEB_CONCURRENCY") { 2 })
 
 
 # Puma can serve each request in a thread from an internal thread pool.
@@ -19,17 +19,6 @@ max_threads_count = Integer(ENV.fetch("RAILS_MAX_THREADS") { 5 })
 min_threads_count = Integer(ENV.fetch("RAILS_MIN_THREADS") { max_threads_count })
 threads min_threads_count, max_threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
-
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RACK_ENV") { "development" }
-
-# Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
@@ -38,11 +27,16 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 preload_app!
 rackup      DefaultRackup
 
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+#
+port        ENV.fetch("PORT") { 3000 }
+
+# Specifies the `environment` that Puma will run in.
+#
+environment ENV.fetch("RACK_ENV") { "development" }
+
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
 end
-
-# Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
